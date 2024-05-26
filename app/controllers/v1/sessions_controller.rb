4752fd5a889249011ff253f7c2be993a4bf954b9
Trung Mai <trung.mai@employmenthero.com>
2024-05-26 20:04:42 +0700

    Implement sign in/out and contacts CRUD

---
+++ app/controllers/v1/sessions_controller.rb
@@ -0,0 +1,21 @@
+class V1::SessionsController < ApplicationController
+
+  def create
+    @user = User.where(email: params[:email]).first
+
+    if @user&.valid_password?(params[:password])
+      render :create, status: :created
+    else
+      head(:unauthorized)
+    end
+  end
+
+  def destroy
+    if current_user&.save
+      head(:ok)
+    else
+      head(:unauthorized)
+    end
+  end
+
+end
--- db/migrate/20240506120452_create_contacts.rb
+++ db/migrate/20240506120452_create_contacts.rb
@@ -4,6 +4,7 @@ class CreateContacts < ActiveRecord::Migration[7.1]
       t.string :first_name
       t.string :last_name
       t.string :email
+
       t.timestamps
     end
   end
